21eb3bd272d0c628eb9a4724c6d9367b1880fb4a
Nanditha G B <nandithagb03@gmail.com>
2023-11-10 14:03:33 +0530

    infinte loop and infinte scroll adding into news

--- src/components/New.js
+++ src/components/New.js
@@ -1,78 +1,108 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
+import React, { Component } from "react";
+import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class New extends Component {
-    articles = [
-        {
-            "source": {
-                "id": "bbc-sport",
-                "name": "BBC Sport"
-            },
-            "author": null,
-            "title": "India bat first against SA in clash of top two",
-            "description": "Follow live text, in-play video clips and radio commentary as India play South Africa in the Men's Cricket World Cup.",
-            "url": "http://www.bbc.co.uk/sport/live/cricket/67278614",
-            "urlToImage": "https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.23.3/images/bbc-sport-logo.png",
-            "publishedAt": "2023-11-05T09:07:21.5146316Z",
-            "content": "India: Rohit Sharma (c), Shubman Gill, Virat Kohli, Shreyas Iyer, KL Rahul (wk), Suryakumar Yadav, Ravindra Jadeja, Mohammed Shami, Jasprit Bumrah, Kuldeep Yadav, Mohammed Siraj \r\nSouth Africa: Quint… [+276 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ]
-    constructor(){
-        super();
-        console.log("hello im a constructor fom news component")
-        this.state={
-            articles:this.articles,
-            loading:false
-        }
-    }
-    componentDidMount(){
-        console.log("cdm")
-    }
+  static defaultProps = {
+    country: "in",
+    pagesize: 5,
+    category: "general",
+  };
+  static propTypes = {
+    country: PropTypes.string,
+    pagesize: PropTypes.number,
+    category: PropTypes.string,
+  };
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+  constructor(props) {
+    super(props);
+
+    console.log(props,'props');
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0,
+    };
+    document.title = `${this.capitalizeFirstLetter(this.props.category )}-News Monkey`;
+  }
+  async updatenews() {
+    this.props.setProgress(10);
+   let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c2ac0897e3e94a9ba3384536e36e555c&page=${this.state.page}&pageSize=${this.props.pagesize}`
+
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData,"parsedData")
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+    this.props.setProgress(100);
+  }
+  async componentDidMount() {
+    this.updatenews();
+  }
+  handlePrevious = async () => {
+   this.setState({page:this.state.page-1});
+   this.updatenews();
+  };
+  handleNext = async () => {
+    this.setState({page:this.state.page+1});
+    this.updatenews();
+  };
+  fetchMoreData=async()=>{
+    this.setState({page:this.state.page+1});
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c2ac0897e3e94a9ba3384536e36e555c&page=${this.state.page}&pageSize=${this.props.pagesize}`
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults
+    });
+  }
   render() {
     return (
-      <div className='container my-3'>
-        <h2>NewsMonkey-Top headline</h2>
+      <>
+      
+        <h1 className="text-center" style={{margin:'35px 0px'}}>News-Top {this.capitalizeFirstLetter(this.props.category)} Headline </h1>
+        {this.state.loading && <Spinner />}
+         <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length!==this.state.totalResults}
+          loader={<Spinner/>}
+          >
+            <div className="container">
         <div className="row">
-        {this.state.articles.map((element)=>{
-            return <div className="col-md-4"  key={element.url}>
-            <Newsitem title={element.title.slice(0,45)} description={element.description.slice(0,88)}
-             imageurl={element.urlToImage}
-             newsUrl={element.url}/>
-            </div>
+          {
+            
+          this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <Newsitem
+                    title={element.title ? element.title : ""}
+                    description={element.description ? element.description : ""}
+                    imageurl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
             })}
-
-        
-        
+      </div>
         </div>
-    </div>
-    )
+      </InfiniteScroll>
+      </>
+    );
   }
 }
 
-export default New
+export default New;
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -3,15 +3,18 @@ import React, { Component } from 'react'
 export class Newsitem extends Component {
     
   render() {
-   let  {title,description,imageurl,newsUrl}=this.props;
+   let  {title,description,imageurl,newsUrl,author,date,source}=this.props;
     return (
       <div className='my-3'>
-                <div className="card" style={{width:"18rem"}}>
-            <img src={imageurl} className="card-img-top" style={{height:"10rem"}} alt="..."/>
+                <div className="card" >
+                  <div style={{display:'flex',justifyContent:'flex-end',position:'absolute',right:'0'}}> <span className="badge rounded-pill bg-danger" >{source}</span>
+</div>
+            <img src={!imageurl?"https://www.livemint.com/lm-img/img/2023/11/06/1600x900/PTI11-06-2023-000041B-0_1699260902194_1699260919277.jpg":imageurl} className="card-img-top" style={{height:"10rem"}} alt="..."/>
             <div className="card-body">
-                <h5 className ="card-title">{title}......</h5>
-                <p className="card-text">{description}.......</p>
-                <a href={newsUrl} target="blank" className="btn btn-sm btn-primary">read More</a>
+                <h5 className ="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <p className="card-text"><small className="text-body-secondary">by {!author?"unknow":author} on {new Date(date).toGMTString()}</small></p>
+                <a href={newsUrl} target="blank" className="btn btn-sm btn-dark ">read More</a>
             </div>
             </div>
       </div>
